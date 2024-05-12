9d308b5a37d4708291451e82b04062a0ad4ac853
Cuco22 <128377272+Cuco22@users.noreply.github.com>
2024-05-12 18:29:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require '/aws_progetto/php/connet.php';
+    require 'aws_progetto/php/connet.php';
     session_start();
 ?>
 
@@ -9,11 +9,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login cliente</title>
-    <link rel="stylesheet" href="/aws_progetto/css/style.css">
+    <link rel="stylesheet" href="aws_progetto/css/style.css">
 </head>
 <body>
     <h1>LOGIN CLIENTE</h1>
-    <form method="post" action="/aws_progetto/php/loign.php"> 
+    <form method="post" action="aws_progetto/php/loign.php"> 
         <input type="email" name="email" placeholder="email" required>
         <input type="password" name="password" placeholder="password" required>
 		<br>
