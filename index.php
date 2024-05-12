e30412f3a385819ef422f0abcabaa4ec30af5baf
Cuco22 <128377272+Cuco22@users.noreply.github.com>
2024-05-12 17:18:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
+    require 'connet.php';
+    session_start();
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login cliente</title>
+    <link rel="stylesheet" href="/aws_progetto/css/style.css">
+</head>
+<body>
+    <h1>LOGIN CLIENTE</h1>
+    <form method="post" action="/aws_progetto/php/loign.php"> 
+        <input type="email" name="email" placeholder="email" required>
+        <input type="password" name="password" placeholder="password" required>
+		<br>
+        <input type="submit" value="login">
+    </form>
 </body>
 </html>
