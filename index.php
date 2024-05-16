72c064b6c124eee214608b561ea5d56b451f0cd9
Cuco22 <128377272+Cuco22@users.noreply.github.com>
2024-05-16 12:30:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ob_start(); // Avvia il buffering dell'output
+
 $host = 'db';
 $username = 'luca';
 $password = 'password';
@@ -8,11 +10,12 @@ $database = 'database';
 $conn = mysqli_connect($host, $username, $password, $database);
 
 if ($conn->connect_error) {
-    
-  die("non riesco a connettermi!! errore -> " . $conn->connect_error);
-
-} else{
-   echo 'ciaoooo sono connesso al database';
+    die("non riesco a connettermi!! errore -> " . $conn->connect_error);
+} else {
+    echo 'ciaoooo sono connesso al database';
     header("Location: lg.php");
+    exit(); // Assicurati di uscire dopo il reindirizzamento
 }
+
+ob_end_flush(); // Termina e invia il buffer di output
 ?>
