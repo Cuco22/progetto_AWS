8f62f57b9ec5fe1bcd0dc067898865f6f93b3948
Cuco22 <128377272+Cuco22@users.noreply.github.com>
2024-05-16 12:40:04 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,16 @@
+<?php
+$host = 'db';
+$username = 'luca';
+$password = 'password';
+$database = 'database';
 
+// Crea una connessione al database
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("non riesco a connettermi!! errore -> " . $conn->connect_error);
+} else {
+    //echo 'ciaoooo sono connesso al database';
+    //exit(); // Assicurati di uscire dopo il reindirizzamento
+}
+?>
