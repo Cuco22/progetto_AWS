264c55de7d64e87d4cd466927e935067bb6e2a84
Cuco22 <128377272+Cuco22@users.noreply.github.com>
2024-05-18 14:53:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login cliente</title>
+	<link rel="icon" href="/aws_progetto/css/icona login.png" type="image/x-icon">
     <link rel="stylesheet" href="/aws_progetto/css/style.css">
 </head>
 <body>
