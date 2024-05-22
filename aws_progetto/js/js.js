01195ea1674ee9325af439a321c3646b0e4e4a76
Cuco22 <128377272+Cuco22@users.noreply.github.com>
2024-05-22 15:21:31 +0200

    Update js.js

--- aws_progetto/js/js.js
+++ aws_progetto/js/js.js
@@ -1,27 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     var emailDisplay = document.getElementById('emailDisplay');
     var email = emailDisplay.getAttribute('data-email');
-    emailDisplay.textContent = email;
+
+    // Trova l'indice del simbolo '@' nell'indirizzo email
+    var atIndex = email.indexOf('@');
+
+    if (atIndex !== -1) {
+        // Estrae la parte di indirizzo email prima del simbolo '@'
+        var displayText = email.substring(0, atIndex);
+        emailDisplay.textContent = displayText;
+    }
 
     var commentForm = document.getElementById('commentForm');
     var commentInput = document.getElementById('commentInput');
     var commentSection = document.getElementById('commentSection');
 
-    document.addEventListener('DOMContentLoaded', function() {
-        var emailDisplay = document.getElementById('emailDisplay');
-        var email = emailDisplay.getAttribute('data-email');
-    
-        //trova l'indice del simbolo '@' nell'indirizzo email
-        var atIndex = email.indexOf('@');
-    
-        if (atIndex !== -1) {
-            //estrae la parte di indirizzo email prima del simbolo '@'
-            var displayText = email.substring(0, atIndex);
-            emailDisplay.textContent = displayText;
-        }
-    });
-
-    //impostazione zona commenti
+    // Impostazione zona commenti
     commentForm.addEventListener('submit', function(event) {
         event.preventDefault(); 
 
