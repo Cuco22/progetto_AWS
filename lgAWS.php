2f4d3470dfaa12ae6cae6ded52e18324fae6c665
Cuco22 <128377272+Cuco22@users.noreply.github.com>
2024-05-22 15:12:26 +0200

    Update lgAWS.php

--- lgAWS.php
+++ lgAWS.php
@@ -11,19 +11,20 @@ if (!isset($_POST['email']) || !isset($_POST['password'])) {
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+// Hash la password
 $password_hashed = md5($password);
 
-//verifica se l'utente esiste nel database
-$query = "SELECT * FROM utente WHERE username = '$email' AND password = '$password'";
+//verifico se l'utente esiste nel database
+$query = "SELECT * FROM utente WHERE email = '$email' AND password = '$password_hashed'";
 $result = $conn->query($query);
 
 if ($result->num_rows > 0) {
-    $_SESSION['logged'] === true;
+    $_SESSION['logged'] = true;
     $_SESSION['email'] = $email;
     header("Location: sitoAWS.php");
     exit();
 } else {
-    echo "L'utente non esiste! Non è possibile eseguire il login :(";
+    $_SESSION['error_message'] = "L'utente non esiste! Non è possibile eseguire il login :(";
     header("Location: index.php");
     exit();
 }
