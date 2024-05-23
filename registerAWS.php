8b9dc0e5408f3747e81f46305da0023e26ab97f8
Cuco22 <128377272+Cuco22@users.noreply.github.com>
2024-05-23 11:22:54 +0200

    Update registerAWS.php

--- registerAWS.php
+++ registerAWS.php
@@ -5,24 +5,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
     
-    // Hash della password
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     
-    // Query di inserimento
     $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
     
-    if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param("ss", $email, $hashed_password);
-        
-        if ($stmt->execute()) {
-            echo "Registrazione avvenuta con successo.";
-            // Redireziona a una pagina di successo o login
-        } else {
-            echo "Errore: " . $stmt->error;
-        }
-        
-        $stmt->close();
+    if($conn->query($sql)===TRUE){
+        header("Location: index.php");
+    }else{
+        echo 'errore';
     }
+
+    $conn->close();
     
     $conn->close();
 }
